a488666b395fa8aa19c59c4cc3dbc2db8aa241a1
ricardojonathanromero <ricardo.jonathan.romero@gmail.com>
2024-04-13 21:46:24 -0600

    feat: declare main with lambda.Start implementation

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/aws/aws-lambda-go/lambda"
+
+/*
+"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+*/
+
+func main() {
+	lambda.Start(nil)
+}
